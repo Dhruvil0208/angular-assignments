78e12b36fa191ca20f7f5b31b435eb5d722286df
Dhruvil0208 <chaudharydhruvil671@gmail.com>
2023-02-17 19:33:08 +0530

    Basic add feature

--- todo-list/src/app/app.component.ts
+++ todo-list/src/app/app.component.ts
@@ -7,4 +7,34 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todo-list';
+  // tasks:any [] =[];
+
+  // addTask(item:string){
+  //   this.tasks.push({id:this.tasks.length.toString(),name:item})
+  //   console.log(this.tasks);
+  // }
+
+  filter: 'all' | 'active' | 'done' = 'all';
+
+  allItems = [
+    { description: 'eat', done: true },
+    { description: 'sleep', done: false },
+    { description: 'play', done: false },
+    { description: 'laugh', done: false },
+  ];
+
+  get items() {
+    if (this.filter === 'all') {
+      return this.allItems;
+    }
+    return this.allItems.filter((item) => this.filter === 'done' ? item.done : !item.done);
+  }
+
+  addItem(description: string) {
+    this.allItems.unshift({
+      description,
+      done: false
+    });
+  }
+ 
 }
