561bb95554d8003331272594efa3e6b92de92751
Dhruvil0208 <chaudharydhruvil671@gmail.com>
2023-02-21 18:47:57 +0530

    local storage is now used

--- todo-list/src/app/app.component.ts
+++ todo-list/src/app/app.component.ts
@@ -9,20 +9,30 @@ export class AppComponent {
   @ViewChild('todoTask') todo!: { nativeElement: { value: string; }; };
   title = 'todo list';
   tasks:any [] =[];
+  editContent:any='';
 
+  constructor(){
+
+  let todolist = localStorage.getItem('todolist');
+  if(todolist){
+    this.tasks= JSON.parse(todolist);
+  }
+
+  }
+  
   addTask(item:string){
     if(item.trim()!='')
     {
       this.tasks.push({id:this.tasks.length,name:item,select:false})
       this.todo.nativeElement.value='';
+      localStorage.setItem('todolist',JSON.stringify(this.tasks))
     }
     else{
       alert('Task cannot be empty')
     }
-
-    console.log(this.tasks);
-    
+    console.log(this.tasks); 
   }
+
 
   onChangeTask($event:any){
     const id =$event.target.value;
@@ -37,6 +47,7 @@ export class AppComponent {
       }
       return d;
     });
+    localStorage.setItem('todolist',JSON.stringify(this.tasks))
     console.log(this.tasks)
   }
 
@@ -45,6 +56,22 @@ export class AppComponent {
     if(index !== -1){
       this.tasks.splice(index,1);
     }
+    localStorage.setItem('todolist',JSON.stringify(this.tasks))
   }
+
+  // editTask(task:any){
+  //   this.editContent = task;
+  // }
+
+  // saveTask(task:any){
+  //   if(this.editContent === task)
+  //   {
+  //     task.name = task.name.trim();
+  //     if(task.name.length === 0){
+  //       this.removeTask(task);
+  //     }
+  //     this.editContent = '';
+  //   }
+  // }
  
 }
